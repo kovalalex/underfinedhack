cbc9d4d89064f1373c1ad1043100243c2416fb72
Екатерина Стукова <stukova.katya@mail.ru>
2022-06-27 08:25:45 +0500

    fix

--- src/main/java/ru/cbr/underfinedhack/entity/DataLoader.java
+++ src/main/java/ru/cbr/underfinedhack/entity/DataLoader.java
@@ -1,6 +1,7 @@
 package ru.cbr.underfinedhack.entity;
 
 import lombok.extern.apachecommons.CommonsLog;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -8,16 +9,18 @@ import org.springframework.stereotype.Component;
 import ru.cbr.underfinedhack.repository.EmployeeRepository;
 
 @Component
+@Slf4j
 public class DataLoader implements CommandLineRunner {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @Value("${data.generation.enabled:false}")
+    @Value("${data.generation.enabled:true}")
     private boolean generateData;
 
     @Override
     public void run(String... args) throws Exception {
         if (generateData) {
+            log.info("data generation started...");
             Employee testEmployee = new Employee();
             testEmployee.setEmail("2@2.ru");
             testEmployee.setFio("testfio");
